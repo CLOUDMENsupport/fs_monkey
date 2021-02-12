f3c57afc581f294edb6c4b5818eef36fd706d129
CLOUDMENsupport <78953631+CLOUDMENsupport@users.noreply.github.com>
2021-02-12 09:39:38 +0100

    Create freshservice.user.js

---
+++ freshservice.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name FreshService CSS Customisations
+// @namespace https://cloudmen.com
+// @version 0.9
+// @description  Adds some style customisations to FreshService
+// @match https://*.freshservice.com/*
+// @copyright 2021, cloudmen.com
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $(".private-note").css("background-color","#ef5456");
+    $(".private-note").css("color","white");
+})();
